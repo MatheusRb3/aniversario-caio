909bc31c977472e9bd21d4d697666f883e1d5870
Matheus <109323270+MatheusRb3@users.noreply.github.com>
2025-06-29 10:33:48 -0300

    Aniversário caio

---
+++ js/script.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', () => {
+      const convite = document.getElementById('container');
+      setTimeout(() => {
+        convite.classList.add('visible');
+      }, 300);
+    });
